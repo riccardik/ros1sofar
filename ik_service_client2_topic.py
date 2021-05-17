d0ac1c766342baac1a4c6eeaa50aeab5819e0903
riky.lastrico@gmail.com <riky.lastrico@gmail.com>
2021-05-17 17:04:43 +0200

    ik service client as topic

--- ik_service_client2_topic.py
+++ ik_service_client2_topic.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2013-2015, Rethink Robotics
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the Rethink Robotics nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+
 
 """
 Baxter RSDK Inverse Kinematics Example
+
+rosrun baxter_examples ik_service_client2_topic.py -l left
+rostopic pub positn_sub geometry_msgs/Point "x: 0.8
+y: 0.8
+z: 0.03" 
+
 """
 import argparse
 import struct
@@ -64,74 +45,45 @@ from baxter_core_msgs.msg import (
 
 
 
+global limb
+global req_pos
+limb="left"
 
-
-def ik_test(limb):
-    rospy.init_node("rsdk_ik_service_client")
+def pose_callback(data):
+    global req_pos
+    req_pos = data
+    print(req_pos)
 
     rs = baxter_interface.RobotEnable(CHECK_VERSION)
     init_state = rs.state().enabled
     left = baxter_interface.Gripper('left', CHECK_VERSION)
     left.open()
 
-    print("insert x")
-    x1=input()
-    print("insert y")
-    y1=input()
-    print("insert z")
-    z1=input()
+    
     ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
     iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
     ikreq = SolvePositionIKRequest()
     hdr = Header(stamp=rospy.Time.now(), frame_id='base')
-    poses = {
-        'left': PoseStamped(
-            header=hdr,
-            pose=Pose(
-                position=Point(
-                    x=x1,
-                    y=y1,
-                    z=z1
-                ),
-                orientation=Quaternion(
-                    x=-0.366894936773,
-                    y=0.885980397775,
-                    z=0.108155782462,
-                    w=0.262162481772,
-                ),
-            ),
-        ),
-        'right': PoseStamped(
-            header=hdr,
-            pose=Pose(
-                position=Point(
-                    x=0.656982770038,
-                    y=-0.852598021641,
-                    z=0.0388609422173,
-                ),
-                orientation=Quaternion(
-                    x=0.367048116303,
-                    y=0.885911751787,
-                    z=-0.108908281936,
-                    w=0.261868353356,
-                ),
-            ),
-        ),
-    }
-    pose1 = PoseStamped(
-            header=hdr,
-            pose=Pose(
-                position=Point(
-                    x=x1,
-                    y=y1,
-                    z=z1
-                ),
-                orientation=Quaternion(
+
+    
+    q1 = Quaternion(
                     x=0.366894936773,
                     y=0.885980397775,
                     z=0.108155782462,
                     w=0.262162481772,
-                ),
+                )
+    q2 = Quaternion(
+                    x=-0.36,
+                    y=0.88,
+                    z=0.10,
+                    w=0.259,
+                )
+   
+    pose1 = PoseStamped(
+            header=hdr,
+            pose=Pose(
+                position=req_pos,
+                orientation=q1,
             ),
         )
 
@@ -139,10 +91,7 @@ def ik_test(limb):
     ikreq.pose_stamp.append(pose1)
     try:
         rospy.wait_for_service(ns, 5.0)
-        ikreq.seed_mode = 0
-        print (ikreq)
         resp = iksvc(ikreq)
-        
     except (rospy.ServiceException, rospy.ROSException), e:
         rospy.logerr("Service call failed: %s" % (e,))
         return 1
@@ -157,7 +106,6 @@ def ik_test(limb):
                     ikreq.SEED_CURRENT: 'Current Joint Angles',
                     ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
                    }.get(resp_seeds[0], 'None')
-        print(ikreq)
         print("SUCCESS - Valid Joint Solution Found from Seed Type: %s" %
               (seed_str,))
         print "------------------"
@@ -200,12 +148,14 @@ def ik_test(limb):
         pub.publish(jointss)
 
         rospy.sleep(1)
+        #open gripper
         pub1 = rospy.Publisher('/robot/left_gripper_controller/joints/l_gripper_r_finger_controller/command', Float64, queue_size=1) 
         gr1=Float64()
         gr1.data= 0       
         while pub1.get_num_connections() < 1:
             it=1
         pub1.publish(gr1)
+        #close gripper
         pub2 = rospy.Publisher('/robot/left_gripper_controller/joints/l_gripper_l_finger_controller/command', Float64, queue_size=1) 
         gr2=Float64()
         gr2.data= 0       
@@ -215,6 +165,12 @@ def ik_test(limb):
     else:
         print("INVALID POSE - No Valid Joint Solution Found.")
 
+def ik_test(limb):
+    rospy.init_node("rsdk_ik_service_client")
+    rospy.Subscriber("position_sub", Point, pose_callback)
+    while 1:
+        rospy.sleep(1)
+
     
 
     return 0
@@ -223,15 +179,7 @@ def ik_test(limb):
 def main():
     """RSDK Inverse Kinematics Example
 
-    A simple example of using the Rethink Inverse Kinematics
-    Service which returns the joint angles and validity for
-    a requested Cartesian Pose.
-
-    Run this example, passing the *limb* to test, and the
-    example will call the Service with a sample Cartesian
-    Pose, pre-defined in the example code, printing the
-    response of whether a valid joint solution was found,
-    and if so, the corresponding joint angles.
+    
     """
     arg_fmt = argparse.RawDescriptionHelpFormatter
     parser = argparse.ArgumentParser(formatter_class=arg_fmt,
